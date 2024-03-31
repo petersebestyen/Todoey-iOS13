939a3bcbe34cec1f17756118874561a78a909b6c
Peter Sebestyen <peter.sebestyen@mac.com>
2024-03-31 12:09:28 +0200

    User defaults

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -10,38 +10,51 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    var itemArray = [Item]() //  = [Item(title:"Find Mike", done:false), Item(title: "Buy Eggs", done:false), Item(title:"Destroy Demogorgon", done:false)]
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
+        if let keys = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = [Item]()
+            for key in keys {
+                let array = key.components(separatedBy: "|")
+                // print(array)
+                itemArray.append(Item(title: array.first ?? "" , done: array.last ?? "" == "TRUE" ? true : false))
+            }
         }
-            
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        saveChanges()
     }
 
     //MARK: - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType   = itemArray[indexPath.row].done ? .checkmark : .none
         return cell
     }
     
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // print(itemArray[indexPath.row])
-        
-        let accessoryType = tableView.cellForRow(at: indexPath)?.accessoryType ?? .none
-        tableView.cellForRow(at: indexPath)?.accessoryType = (accessoryType == .none ? .checkmark : .none)
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+        itemArray[indexPath.row].done.toggle()
+        saveChanges()
+
+        tableView.reloadData()
+        // tableView.cellForRow(at: indexPath)?.accessoryType = (itemArray[indexPath.row].done ? .checkmark : .none)
+        // tableView.deselectRow(at: indexPath, animated: true)
     }
     
     //MARK: - Add new item
@@ -50,8 +63,10 @@ class TodoListViewController: UITableViewController {
         
         let alert  = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            self.itemArray.append(textField.text ?? "New Item")
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.itemArray.append(Item(title:textField.text ?? "New Item", done: false))
+            self.saveChanges()
+            // print(self.itemArrayToString())
             
             self.tableView.reloadData()
         }
@@ -61,6 +76,20 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func itemArrayToString() -> [String] {
+        var result = [String]()
+        guard !itemArray.isEmpty else { return result }
+        
+        for item in itemArray {
+            result.append(item.title + "|" + (item.done ? "TRUE" : "FALSE"))
+        }
+        return result
+    }
+    
+    func saveChanges() {
+        defaults.set(itemArrayToString(), forKey: "TodoListArray")
     }
 }
 
--- Todoey/Model/Item.swift
+++ Todoey/Model/Item.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class Item: Codable {
+    var title: String
+    var done:  Bool
+    
+    init(title: String, done: Bool) {
+        self.title = title
+        self.done = done
+    }
+}
